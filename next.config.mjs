e859b67a59db90ce6df3e718b620ea4234fd0ea8
Amarjargal <amarjargal.ba@ictgroup.mn>
2025-11-09 12:22:15 +0800

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path',
+                destination: 'http://103.50.205.185:3000/api/v1/:path*', // дотоод HTTP
+            },
+        ];
+    },
+};
 
 export default nextConfig;
