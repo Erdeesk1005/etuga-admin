218b45b577364a7bbf386c6504f7420507fca9cc
Erdeesk1005 <erdeesks@gmail.com>
2025-11-18 17:54:10 +0800

    t

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://api/etuga.mn/api/v1/';
+export const BASE_URL = 'https://api.etuga.mn/api/v1/';
 
-export const FILE_URL = 'https://api/etuga.mn';
+export const FILE_URL = 'https://api.etuga.mn';
